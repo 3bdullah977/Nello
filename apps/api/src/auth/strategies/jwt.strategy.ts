2087484d648044bcc4f63ead9607a9ab3d92b75f
ahmedgamer4 <ahmedalseidy4@gmail.com>
2023-11-10 20:52:44 +0200

    fix typo

--- apps/api/src/auth/strategies/jwt.strategy.ts
+++ apps/api/src/auth/strategies/jwt.strategy.ts
@@ -1,13 +1,11 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { AuthService } from '../auth.service';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export default class LocalStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(
-    private readonly authService: AuthService,
     private readonly configService: ConfigService,
   ) {
     super({
