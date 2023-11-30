7d08a21e804cc96cba85f649ef66cea5322d35de
ahmedgamer4 <ahmedalseidy4@gmail.com>
2023-11-30 09:36:50 +0200

    Add: columns crud

--- apps/api/src/modules/columns/columns.controller.ts
+++ apps/api/src/modules/columns/columns.controller.ts
@@ -1,34 +1,78 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+  Query,
+  ParseIntPipe,
+  HttpStatus,
+  UseGuards,
+} from '@nestjs/common';
 import { ColumnsService } from './columns.service';
 import { CreateColumnDto } from './dto/create-column.dto';
 import { UpdateColumnDto } from './dto/update-column.dto';
+import { ok, res } from '@/utils/response-helper';
+import { QueryColumnDto } from './dto/query-column.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import LocalAuthGuard from '../auth/guards/jwt.guard';
 
+@ApiTags('Columns')
+@ApiBearerAuth()
 @Controller('columns')
+@UseGuards(LocalAuthGuard)
 export class ColumnsController {
   constructor(private readonly columnsService: ColumnsService) {}
 
   @Post()
-  create(@Body() createColumnDto: CreateColumnDto) {
-    return this.columnsService.create(createColumnDto);
+  @HttpCode(201)
+  async create(@Body() createColumnDto: CreateColumnDto) {
+    if (!createColumnDto) return null;
+    const column = await this.columnsService.create(createColumnDto);
+    return ok('Created column successfully', column, true);
   }
 
   @Get()
-  findAll() {
-    return this.columnsService.findAll();
+  async findAll(@Query() query: QueryColumnDto) {
+    const page = query.page ?? 1;
+    let limit = query.limit ?? 10;
+    if (limit > 50) {
+      limit = 50;
+    }
+    const columns = await this.columnsService.findAll(page, limit);
+    return ok('Found columns successfully', columns);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.columnsService.findOne(+id);
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    const column = await this.columnsService.findOne(id);
+    if (!column) return res('No column with this id', HttpStatus.NOT_FOUND);
+
+    return ok('Found column successfully', column);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateColumnDto: UpdateColumnDto) {
-    return this.columnsService.update(+id, updateColumnDto);
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateColumnDto: UpdateColumnDto,
+  ) {
+    const column = await this.columnsService.findOne(id);
+    if (!column) return res('No column with this id', HttpStatus.NOT_FOUND);
+
+    return ok(
+      'Updated column successfully',
+      await this.columnsService.update(id, updateColumnDto),
+    );
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.columnsService.remove(+id);
+  async remove(@Param('id', ParseIntPipe) id: number) {
+    const column = await this.columnsService.remove(id);
+    if (!column) return res('No column with this id', HttpStatus.NOT_FOUND);
+
+    return ok('Removed column successfully', column);
   }
 }
--- apps/api/src/modules/columns/dto/create-column.dto.ts
+++ apps/api/src/modules/columns/dto/create-column.dto.ts
@@ -1,1 +1,27 @@
-export class CreateColumnDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsAlphanumeric,
+  IsNotEmpty,
+  IsNumber,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateColumnDto {
+  @ApiProperty()
+  @IsAlphanumeric()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(100)
+  name: string;
+
+  @ApiProperty()
+  @IsNumber()
+  @IsNotEmpty()
+  boardId: number;
+
+  @ApiProperty()
+  @IsNumber()
+  @IsNotEmpty()
+  position: number;
+}
