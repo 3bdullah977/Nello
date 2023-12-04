e4d58c70b34b936d39027530025357472450c167
ahmedgamer4 <ahmedalseidy4@gmail.com>
2023-12-04 19:54:18 +0200

    Remove: uploads controller

--- apps/api/src/modules/uploads/uploads.module.ts
+++ apps/api/src/modules/uploads/uploads.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UploadsService } from './uploads.service';
-import { UploadsController } from './uploads.controller';
 
 @Module({
   providers: [UploadsService],
-  controllers: [UploadsController]
 })
 export class UploadsModule {}
