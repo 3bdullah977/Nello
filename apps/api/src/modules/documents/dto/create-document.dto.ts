d6833453827161886b4a125484c6db433d3feb6e
ahmedgamer4 <ahmedalseidy4@gmail.com>
2023-12-12 13:04:12 +0200

    Modify: made content optional

--- apps/api/src/modules/documents/dto/create-document.dto.ts
+++ apps/api/src/modules/documents/dto/create-document.dto.ts
@@ -1,9 +1,22 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateDocumentDto {
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(100)
+  name: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
   content: string;
 }
